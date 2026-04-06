99590cc6cd424dbcedb7efaa97fce6cf9874c846
emmanuelbilla <immanuelbilla33@gmail.com>
2026-04-06 11:21:32 +0000

    Correcting database migrations conflict

--- accounts/urls.py
+++ accounts/urls.py
@@ -13,3 +13,15 @@ router.register('users', AccountViewSet, basename='accounts')
 urlpatterns = [
     path('', include(router.urls)),
 ]
+'''
+Adds up endpoints for urls including:
+users/$ [name='accounts-list'] - for listing and creating accounts
+^users\.(?P<format>[a-z0-9]+)/?$ [name='accounts-list'] - for listing and creating accounts with format suffix
+^users/login/$ [name='accounts-login'] - for logging in users
+^users/login\.(?P<format>[a-z0-9]+)/?$ [name='accounts-login'] - for logging in users with format suffix
+^users/logout/$ [name='accounts-logout'] - for logging out users
+^users/logout\.(?P<format>[a-z0-9]+)/?$ [name='accounts-logout'] - for logging out users with format suffix
+^users/(?P<pk>[^/.]+)/$ [name='accounts-detail'] - for retrieving, updating, or deleting a specific account
+^users/(?P<pk>[^/.]+)\.(?P<format>[a-z0-9]+)/?$ [name='accounts-detail'] - for retrieving, updating, or deleting a specific account with format suffix
+[name='api-root'] 
+'''
--- users/admin.py
+++ users/admin.py
@@ -1,4 +1,4 @@
-from django.contrib import admin
+'''from django.contrib import admin
 from .models.new_user import NewUser
 
 # Register your models here.
@@ -6,3 +6,4 @@ from .models.new_user import NewUser
 class NewUserAdmin(admin.ModelAdmin):
     list_display = ('id', 'username', 'email', 'first_name', 'last_name', 'created_at', 'updated_at', 'is_active', 'is_staff', 'is_superuser')
     search_fields = ('username', 'email', 'first_name', 'last_name')
+    '''
